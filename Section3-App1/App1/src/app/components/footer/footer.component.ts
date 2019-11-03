a104b858fc9cb2cfcd11c827787514b047c853b6
JonathanM2ndoza <jonathan.m2ndoza@gmail.com>
2019-11-03 19:06:59 -0400

    Component

---
+++ Section3-App1/App1/src/app/components/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent{
+
+  year: number;
+
+  constructor(){
+    this.year = new Date().getFullYear() + 1;
+  }
+
+}
