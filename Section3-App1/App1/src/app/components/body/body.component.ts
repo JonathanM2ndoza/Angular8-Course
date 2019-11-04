3699885c6500c376e73c56638e126d4346b18aa5
JonathanM2ndoza <jonathan.m2ndoza@gmail.com>
2019-11-03 20:58:48 -0400

    Directives NgIf, NgFor

--- Section3-App1/App1/src/app/components/body/body.component.ts
+++ Section3-App1/App1/src/app/components/body/body.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
     templateUrl: './body.component.html'
 })
 export class BodyComponent{
+    show = true;
+    sentence: any = {
+        message: 'Learn one way to build applications with Angular and reuse your code and abilities to build apps for any deployment target.',
+        author: 'Angular'
+    };
+    pets: string[] = ['Amber', 'Noche', 'Dia'];
 }
