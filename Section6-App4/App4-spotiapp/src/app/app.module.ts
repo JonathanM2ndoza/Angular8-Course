4138cc0b8df4caf3502730ed267e8599db6b0f35
JonathanM2ndoza <jonathan.m2ndoza@gmail.com>
2019-11-09 22:41:47 -0400

    HTTPClient

---
+++ Section6-App4/App4-spotiapp/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+import { ROUTES } from './app.routes';
+import { HttpClient } from 'selenium-webdriver/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot( ROUTES, {useHash: true} )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
