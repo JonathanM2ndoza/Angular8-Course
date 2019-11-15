fdfe0bbd107b35359e26ce2a277a184926b76396
JonathanM2ndoza <jonathan.m2ndoza@gmail.com>
2019-11-14 22:13:45 -0400

    Ionic: Pipe Pure

---
+++ Section7-App5/App5-WishList/src/app/pipes/filter-completed.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { List } from '../models/list.model';
+
+@Pipe({
+  name: 'filterCompleted',
+  pure: false
+})
+export class FilterCompletedPipe implements PipeTransform {
+
+  transform(list: List[], done: boolean = true): List[] {
+    return list.filter( dataList => dataList.done === done);
+  }
+
+}
---
+++ Section7-App5/App5-WishList/src/app/pipes/pipes.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { FilterCompletedPipe } from './filter-completed.pipe';
+
+@NgModule({
+  declarations: [FilterCompletedPipe],
+  exports: [FilterCompletedPipe]
+})
+export class PipesModule { }
