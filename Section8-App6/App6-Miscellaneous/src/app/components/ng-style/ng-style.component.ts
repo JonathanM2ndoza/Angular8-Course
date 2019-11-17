c52f7d881e8f835bd9e55ab201678889ad52dcd1
JonathanM2ndoza <jonathan.m2ndoza@gmail.com>
2019-11-16 21:04:06 -0400

    Angular: ngStyle, style

---
+++ Section8-App6/App6-Miscellaneous/src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  template: `
+    <p [style.fontSize.px]="size" >
+      Hello World.. this is a tag
+    </p>
+
+    <button class="btn btn-primary " (click)="size = size + 5 ">
+      <i class="fa fa-plus"></i>
+    </button>
+    <button class="btn btn-primary" (click)="size = size - 5 ">
+    <i class="fa fa-minus"></i>
+  </button>
+  `,
+  styles: []
+})
+export class NgStyleComponent implements OnInit {
+
+  size:number = 30;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
