281c81228288d7669abfd7fc086fdf16779f5b9b
JonathanM2ndoza <jonathan.m2ndoza@gmail.com>
2019-11-18 21:31:18 -0400

    Angular: Child Routes

--- Section8-App6/App6-Miscellaneous/src/app/app.routes.ts
+++ Section8-App6/App6-Miscellaneous/src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UserComponent } from './components/user/user.component';
+import { NewUserComponent } from './components/user/new-user.component';
+import { UpdateUserComponent } from './components/user/update-user.component';
+import { GetUserComponent } from './components/user/get-user.component';
 
 const ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'user', component: UserComponent },
+    {
+        path: 'user/:id',
+        component: UserComponent,
+        children: [
+            { path: 'new', component: NewUserComponent },
+            { path: 'update', component: UpdateUserComponent },
+            { path: 'get', component: GetUserComponent },
+            { path: '**', pathMatch: 'full', redirectTo: 'new' }
+        ]
+    },
     { path: '', pathMatch: 'full', redirectTo: 'home' },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
