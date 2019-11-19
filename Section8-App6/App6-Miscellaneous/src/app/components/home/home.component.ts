99746d3c0dc590dc7b1de22d6bc1eab32d5a4438
JonathanM2ndoza <jonathan.m2ndoza@gmail.com>
2019-11-18 22:47:41 -0400

    Angular: Lifecycle

--- Section8-App6/App6-Miscellaneous/src/app/components/home/home.component.ts
+++ Section8-App6/App6-Miscellaneous/src/app/components/home/home.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit,
+         AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   template: `
   <hr>
   <app-ng-style></app-ng-style>
+  <br>
   
   <hr>
   <app-css></app-css>
+  <br>
   <p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -15,6 +18,7 @@ import { Component, OnInit } from '@angular/core';
  
   <hr>
   <app-class></app-class>
+  <br>
 
   <hr>
   <p [appRemark]="'orange'">
@@ -22,16 +26,43 @@ import { Component, OnInit } from '@angular/core';
   </p>
 
   <hr>
+  <br>
   <app-ng-switch></app-ng-switch>
 
   `,
   styles: []
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnChanges, DoCheck, AfterContentInit,
+AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() { 
+    console.log('constructor');
   }
 
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+  ngOnChanges() {
+    console.log('ngOnChanges');
+    
+  }
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
+  }
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
+  
 }
