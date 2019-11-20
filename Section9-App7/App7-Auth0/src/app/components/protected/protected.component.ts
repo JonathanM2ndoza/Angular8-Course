632111691cd13c51cafc78e1da8771b6fe384e1d
JonathanM2ndoza <jonathan.m2ndoza@gmail.com>
2019-11-19 21:25:31 -0400

    Angular: guard

--- Section9-App7/App7-Auth0/src/app/components/protected/protected.component.ts
+++ Section9-App7/App7-Auth0/src/app/components/protected/protected.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-protected',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProtectedComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
   }
