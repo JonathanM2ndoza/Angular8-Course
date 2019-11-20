cfe028828ecef874efeed5a260e8ef8ce5c0f53c
JonathanM2ndoza <jonathan.m2ndoza@gmail.com>
2019-11-19 21:02:08 -0400

    Angular: guard

--- Section9-App7/App7-Auth0/src/app/app-routing.module.ts
+++ Section9-App7/App7-Auth0/src/app/app-routing.module.ts
@@ -5,10 +5,16 @@ import { PricesComponent } from './components/prices/prices.component';
 import { ProtectedComponent } from './components/protected/protected.component';
 import { CallbackComponent } from './components/callback/callback.component';
 
+import { AuthGuard } from './services/auth.guard';
+
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'prices', component: PricesComponent},
-  { path: 'protected', component: ProtectedComponent},
+  {
+    path: 'protected',
+    component: ProtectedComponent,
+    canActivate: [AuthGuard]
+  },
   { path: 'callback', component: CallbackComponent},
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
