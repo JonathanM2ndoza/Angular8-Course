91151b8de7a5e639bd1f9bcfceb2254774ba215c
JonathanM2ndoza <jonathan.m2ndoza@gmail.com>
2019-11-22 12:30:19 -0400

    Angular: Firebase Auth REST API

--- Section10-App8/App8-Login/src/app/services/auth.service.ts
+++ Section10-App8/App8-Login/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +10,17 @@ export class AuthService {
   private endPoint = 'https://identitytoolkit.googleapis.com/v1/accounts:';
   private apiKey = 'AIzaSyAvbp7iczZgZLLZbuWlO5ymJm5dNZJg3MU';
 
+  idToken: string;
+
   // Create new user
   // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
 
   // Login user
   // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) { 
+    this.getToken();
+  }
 
   logout() {
 
@@ -30,8 +35,12 @@ export class AuthService {
     return this.httpClient.post(
       `${this.endPoint}signInWithPassword?key=${this.apiKey}`,
       bodyPayload
+    ).pipe (
+      map( resp => {
+        this.saveToken(resp['idToken']);
+        return resp;
+      })
     );
-
   }
 
   signUp(user: User) {
@@ -43,7 +52,26 @@ export class AuthService {
     return this.httpClient.post(
       `${this.endPoint}signUp?key=${this.apiKey}`,
       bodyPayload
+    ).pipe (
+      map( resp => {
+        this.saveToken(resp['idToken']);
+        return resp;
+      })
     );
   }
 
+  private saveToken(idToken: string) {
+
+    this.idToken = idToken;
+    localStorage.setItem('idToken', idToken);
+  }
+
+  private getToken(){
+    if (localStorage.getItem('idToken')) {
+      this.idToken = localStorage.getItem('idToken');
+    } else {
+      this.idToken = '';
+    }
+    return this.idToken;
+  }
 }
