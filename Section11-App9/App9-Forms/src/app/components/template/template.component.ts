6f0ed1f4d7b2e8bebfd6711be865b4417da33e39
JonathanM2ndoza <jonathan.m2ndoza@gmail.com>
2019-11-25 16:02:10 -0400

    Angular: NgForm

--- Section11-App9/App9-Forms/src/app/components/template/template.component.ts
+++ Section11-App9/App9-Forms/src/app/components/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  user:Object = {
+    name: 'Angelica',
+    surname: 'Mendoza',
+    email: 'am@gmail.com'
+  };
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  save(form: NgForm) {
+    console.log('ngForm', form);
+    console.log('value form', form.value);
+    console.log('user', this.user);
   }
 
 }
