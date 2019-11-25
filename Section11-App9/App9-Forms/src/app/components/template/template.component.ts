a0a544582c13634f7fda6c39136f397509967c34
JonathanM2ndoza <jonathan.m2ndoza@gmail.com>
2019-11-25 15:26:05 -0400

    Angular: Forms

---
+++ Section11-App9/App9-Forms/src/app/components/template/template.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: []
+})
+export class TemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
