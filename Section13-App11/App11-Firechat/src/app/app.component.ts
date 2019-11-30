56df78b1d70ef169912def3708c2003ab5670faa
JonathanM2ndoza <jonathan.m2ndoza@gmail.com>
2019-11-30 15:10:28 -0400

    Angular: AngularFire + Cloud Firestore

---
+++ Section13-App11/App11-Firechat/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public chats: Observable<any[]>;
+  constructor(db: AngularFirestore) {
+    this.chats = db.collection('chats').valueChanges();
+  }
+}
---
+++ Section13-App11/App11-Firechat/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCCLBODFchR1MS42cR9xfaZAnUaO7OvU7Y',
+    authDomain: 'app-firechat-10963.firebaseapp.com',
+    databaseURL: 'https://app-firechat-10963.firebaseio.com',
+    projectId: 'app-firechat-10963',
+    storageBucket: 'app-firechat-10963.appspot.com',
+    messagingSenderId: '455603367303',
+    appId: '1:455603367303:web:40e0ec6e2c704e2e6db722',
+    measurementId: 'G-4H5PDK88LR'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
