12b1531c4aa494e249cac16198128082691a0d8c
JonathanM2ndoza <jonathan.m2ndoza@gmail.com>
2019-12-16 21:37:51 -0400

    Angular: Cloud Functions for Firebase - Backend

--- Section23-App19/App19-Backend/functions/src/index.ts
+++ Section23-App19/App19-Backend/functions/src/index.ts
@@ -1,5 +1,17 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
+import * as express from 'express';
+import * as cors from 'cors';
+
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://app-vote-e2401.firebaseio.com"
+});
+
+const db = admin.firestore();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -8,3 +20,30 @@ export const helloWorld = functions.https.onRequest((request, response) => {
         mensaje: 'Hello from firebase-function JM!!!'
        });
    })
+
+export const getGOTY = functions.https.onRequest( async(request, response) => {
+    
+     //const nombre = request.query.nombre || 'Sin Nombre';
+
+    const gotyRef  = db.collection('goty');
+    const docsSnap = await gotyRef.get();
+    const juegos   = docsSnap.docs.map( doc => doc.data() );
+
+    response.json( juegos );
+});
+
+// Express
+const app = express();
+app.use( cors({ origin: true }) );
+
+app.get('/goty', async(req, res) => {
+
+    const gotyRef  = db.collection('goty');
+    const docsSnap = await gotyRef.get();
+    const juegos   = docsSnap.docs.map( doc => doc.data() );
+
+    res.json( juegos );
+});
+
+
+export const api = functions.https.onRequest( app );
